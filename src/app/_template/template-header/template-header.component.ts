eb581b7dff014aa1453ee8e9dd0eac583e253656
OliverZott <zott_oliver@web.de>
2020-08-26 14:06:32 +0200

    Added - some fooling around with @component annotation

--- src/app/_template/template-header/template-header.component.ts
+++ src/app/_template/template-header/template-header.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-template-header',
+  // template: '<h2>Hallo</h2> <button>blub</button>'
   templateUrl: './template-header.component.html',
   styleUrls: ['./template-header.component.sass']
 })
